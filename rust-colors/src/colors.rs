79f612ec508a3a47999179a0435213d3c4f4cf75
RedIsGaming <jaredhofman@protonmail.com>
2024-06-09 20:58:32 +0200

    feat: Seperated Colors and added comments

---
+++ rust-colors/src/colors.rs
@@ -0,0 +1,28 @@
+macro_rules! colors {
+    ($($name:ident, $value:expr);*;) => {
+        #[derive(Debug, PartialEq)]
+        pub enum Colors {
+            $($name,)*
+        }
+
+        impl Colors {
+            pub fn assign(&self) -> &str {
+                match *self {
+                    $(Colors::$name => $value,)*
+                }
+            }
+        }
+    };
+}
+
+colors! {
+    Black, "[30m";
+    Red, "[31m";
+    Green, "[32m";
+    Yellow, "[33m";
+    Blue, "[34m";
+    Purple, "[35m";
+    Cyan, "[36m";
+    White, "[37m";
+    Default, "[0m";
+}
--- rust-colors/src/lib.rs
+++ rust-colors/src/lib.rs
@@ -1,82 +1,84 @@
-use std::fmt;
+//! # rust-colors
+//!
+//! `rust-colors` is a library for parsing ANSI Strings to colors. 
+//! 
+//! ## Example
+//! ```
+//! use rust_colors::{Ansi, Color};
+//! 
+//! fn my_function() {
+//!     let colors = Ansi;
+//!    
+//!     println!(
+//!         "Roses are {}, the sky is {}, I like {} vegetables, but the color {} is even better!",
+//!         colors.red("red"),
+//!         colors.blue("blue"),
+//!         colors.green("green"),
+//!         colors.black("black"),
+//!     );
+//! }
+//! 
+//! ```
+//! 
+//! # Github
+//! [Git repo RedIsGaming rust-colors](https://github.com/RedIsGaming/rust-colors)
 
-macro_rules! colors {
-    ($($name:ident, $value:expr);*;) => {
-        #[derive(Debug, PartialEq)]
-        enum Colors {
-            $($name,)*
+pub use self::ansi_color::{Ansi, Color};
+mod colors;
+
+pub mod ansi_color {
+    use std::fmt;
+    use crate::colors::Colors;
+
+    #[derive(Debug)]
+    pub struct Ansi;
+
+    pub trait Color<T: fmt::Debug> {
+        type Transform;
+
+        fn black(&self, txt: T) -> Self::Transform;
+        fn red(&self, txt: T) -> Self::Transform;
+        fn green(&self, txt: T) -> Self::Transform;
+        fn yellow(&self, txt: T) -> Self::Transform;
+        fn blue(&self, txt: T) -> Self::Transform;
+        fn purple(&self, txt: T) -> Self::Transform;
+        fn cyan(&self, txt: T) -> Self::Transform;
+        fn white(&self, txt: T) -> Self::Transform;
+    }
+
+    impl<T: fmt::Debug> Color<T> for Ansi {
+        type Transform = String;
+
+        fn black(&self, txt: T) -> Self::Transform {
+            format!("\x1b{}{:?}\x1b{}", Colors::assign(&Colors::Black), txt, Colors::assign(&Colors::Default))
         }
 
-        impl Colors {
-            fn assign(&self) -> &str {
-                match *self {
-                    $(Colors::$name => $value,)*
-                }
-            }
+        fn red(&self, txt: T) -> Self::Transform {
+            format!("\x1b{}{:?}\x1b{}", Colors::assign(&Colors::Red), txt, Colors::assign(&Colors::Default))
         }
-    };
-}
 
-colors! {
-    Black, "[30m";
-    Red, "[31m";
-    Green, "[32m";
-    Yellow, "[33m";
-    Blue, "[34m";
-    Purple, "[35m";
-    Cyan, "[36m";
-    White, "[37m";
-    Default, "[0m";
-}
+        fn green(&self, txt: T) -> Self::Transform {
+            format!("\x1b{}{:?}\x1b{}", Colors::assign(&Colors::Green), txt, Colors::assign(&Colors::Default))
+        }
 
-#[derive(Debug)]
-pub struct Ansi;
+        fn yellow(&self, txt: T) -> Self::Transform {
+            format!("\x1b{}{:?}\x1b{}", Colors::assign(&Colors::Yellow), txt, Colors::assign(&Colors::Default))
+        }
 
-pub trait Color<T: fmt::Debug> {
-    type Transform;
+        fn blue(&self, txt: T) -> Self::Transform {
+            format!("\x1b{}{:?}\x1b{}", Colors::assign(&Colors::Blue), txt, Colors::assign(&Colors::Default))
+        }
 
-    fn black(&self, txt: T) -> Self::Transform;
-    fn red(&self, txt: T) -> Self::Transform;
-    fn green(&self, txt: T) -> Self::Transform;
-    fn yellow(&self, txt: T) -> Self::Transform;
-    fn blue(&self, txt: T) -> Self::Transform;
-    fn purple(&self, txt: T) -> Self::Transform;
-    fn cyan(&self, txt: T) -> Self::Transform;
-    fn white(&self, txt: T) -> Self::Transform;
-}
+        fn purple(&self, txt: T) -> Self::Transform {
+            format!("\x1b{}{:?}\x1b{}", Colors::assign(&Colors::Purple), txt, Colors::assign(&Colors::Default))
+        }
 
-impl<T: fmt::Debug> Color<T> for Ansi {
-    type Transform = String;
+        fn cyan(&self, txt: T) -> Self::Transform {
+            format!("\x1b{}{:?}\x1b{}", Colors::assign(&Colors::Cyan), txt, Colors::assign(&Colors::Default))
+        }
 
-    fn black(&self, txt: T) -> Self::Transform {
-        format!("\x1b{}{:?}\x1b{}", Colors::assign(&Colors::Black), txt, Colors::assign(&Colors::Default))
-    }
-
-    fn red(&self, txt: T) -> Self::Transform {
-        format!("\x1b{}{:?}\x1b{}", Colors::assign(&Colors::Red), txt, Colors::assign(&Colors::Default))
-    }
-
-    fn green(&self, txt: T) -> Self::Transform {
-        format!("\x1b{}{:?}\x1b{}", Colors::assign(&Colors::Green), txt, Colors::assign(&Colors::Default))
-    }
-
-    fn yellow(&self, txt: T) -> Self::Transform {
-        format!("\x1b{}{:?}\x1b{}", Colors::assign(&Colors::Yellow), txt, Colors::assign(&Colors::Default))
-    }
-
-    fn blue(&self, txt: T) -> Self::Transform {
-        format!("\x1b{}{:?}\x1b{}", Colors::assign(&Colors::Blue), txt, Colors::assign(&Colors::Default))
-    }
-
-    fn purple(&self, txt: T) -> Self::Transform {
-        format!("\x1b{}{:?}\x1b{}", Colors::assign(&Colors::Purple), txt, Colors::assign(&Colors::Default))
-    }
-
-    fn cyan(&self, txt: T) -> Self::Transform {
-        format!("\x1b{}{:?}\x1b{}", Colors::assign(&Colors::Cyan), txt, Colors::assign(&Colors::Default))
-    }
-
-    fn white(&self, txt: T) -> Self::Transform {
-        format!("\x1b{}{:?}\x1b{}", Colors::assign(&Colors::White), txt, Colors::assign(&Colors::Default))
+        fn white(&self, txt: T) -> Self::Transform {
+            format!("\x1b{}{:?}\x1b{}", Colors::assign(&Colors::White), txt, Colors::assign(&Colors::Default))
+        }
     }
 }
